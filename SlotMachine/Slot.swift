02de3f25d5d9f19f4ffb899fc351a0261bcb1c6f
Korey Powell <korey@vagabonddream.com>
2014-11-12 20:16:59 +0100

    Complete
    Completed lecture

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Sigyn on 11/11/14.
+//  Copyright (c) 2014 Vagabond Dream. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
